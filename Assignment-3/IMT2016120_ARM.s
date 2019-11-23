fa6999f140ba7d4df6dcdadb2f2d0d6257d2c2e5
srirammarisetty19 <39585340+srirammarisetty19@users.noreply.github.com>
2019-11-23 18:59:50 +0530

    Update IMT2016120_ARM.s

--- Assignment-3/IMT2016120_ARM.s
+++ Assignment-3/IMT2016120_ARM.s
@@ -1,5 +1,5 @@
 ; Sriram Marisetty C:\Users\Sriram M\Desktop\C-7\Arm\kei\Assignment-3\Q1	
- 	 area     appcode, CODE, READONLY
+	 area     appcode, CODE, READONLY
 	 IMPORT printMsg
 	 IMPORT printNewLine
 	 IMPORT printGate
@@ -29,9 +29,58 @@ loop VMOV.F32 s6,s0
 	 LTORG
 	
 	 ENDFUNC
+
+	   
+	   EXPORT __GateOr	 
+__GateOr function
+	
+	   MOV r4,r14			
+	   
+	   ; Converting into floats here below 
+	   VCVTR.S32.F32 s21,s7
+	   VCVTR.S32.F32 s22,s8
+	   VCVTR.S32.F32 s23,s9
+	   
+	   ; weights
+	   VLDR.F32 s11, =3.0
+	   VLDR.F32 s12, =3.0
+	   VLDR.F32 s13, =3.0
+	   VLDR.F32 s14, =-2.0
+	   VMUL.F32 s11,s11,s7 ; weight1*x1
+	   VMUL.F32 s12,s12,s8 ; weight2*x2
+	   VMUL.F32 s13,s13,s9 ; weight3*x3
+	   VMUL.F32 s14,s14,s10 ; weight4*bias
+	   
+	   ;adding S11, S12, S13 , S14
+	   VADD.F32 s15,s11,s12 ;
+	   VADD.F32 s15,s15,s13 ;
+	   VADD.F32 s15,s15,s14 ;
+	   VMOV.F32 s2,s15
+	   
+	   
+	   ; Printing the input and the out put for checking the values correct or not
+	   ; The output is in S27
+	   BL __sigmoid
+	   VCVTR.S32.F32 s27,s6
+	   VMOV.F32 r0,s21
+	   BL printMsg
+	   VMOV.F32 r0,s22
+	   BL printMsg
+	   VMOV.F32 r0,s23
+	   BL printMsg
+	   VMOV.F32 r0,s27
+	   BL printMsg
+	   BL printNewLine 
+	   MOV r14,r4
+	   BX lr
+	   LTORG
+	   ENDFUNC
+
+	 
 	 EXPORT __GateAnd	 
+
 __GateAnd function
-	   MOV r5,r14
+	   MOV r4,r14
 	   VCVTR.S32.F32 s21,s7
 	   VCVTR.S32.F32 s22,s8
 	   VCVTR.S32.F32 s23,s9
@@ -45,11 +94,14 @@ __GateAnd function
 	   VMUL.F32 s13,s13,s9 		; weight3*x3
 	   VMUL.F32 s14,s14,s10	 	; weight4*bias
 	   
+	   ;adding S11, S12, S13 , S14
 	   VADD.F32 s15,s11,s12 
 	   VADD.F32 s15,s15,s13 
 	   VADD.F32 s15,s15,s14 
 	   VMOV.F32 s2,s15
-	   
+
+	   ; Printing the input and the out put for checking the values correct or not
+	   ; The output is in S26
 	   BL __sigmoid
 	   VCVTR.S32.F32 s26,s6
 	   VMOV.F32 r0,s21
@@ -61,56 +113,21 @@ __GateAnd function
 	   VMOV.F32 r0,s26
 	   BL printMsg
 	   BL printNewLine
-	   MOV r14,r5
+	   MOV r14,r4
 	   BX lr
 	   LTORG
 
-	   ENDFUNC
-	   
-	   EXPORT __GateOr	 
-__GateOr function
-	   MOV r5,r14
-	   VCVTR.S32.F32 s21,s7
-	   VCVTR.S32.F32 s22,s8
-	   VCVTR.S32.F32 s23,s9
-	   ; weights
-	   VLDR.F32 s11, =3.0
-	   VLDR.F32 s12, =3.0
-	   VLDR.F32 s13, =3.0
-	   VLDR.F32 s14, =-2.0
-	   VMUL.F32 s11,s11,s7 ; weight1*x1
-	   VMUL.F32 s12,s12,s8 ; weight2*x2
-	   VMUL.F32 s13,s13,s9 ; weight3*x3
-	   VMUL.F32 s14,s14,s10 ; weight4*bias
-	   
-	   VADD.F32 s15,s11,s12 ;
-	   VADD.F32 s15,s15,s13 ;
-	   VADD.F32 s15,s15,s14 ;
-	   VMOV.F32 s2,s15
-	   
-	   BL __sigmoid
-	   VCVTR.S32.F32 s27,s6
-	   VMOV.F32 r0,s21
-	   BL printMsg
-	   VMOV.F32 r0,s22
-	   BL printMsg
-	   VMOV.F32 r0,s23
-	   BL printMsg
-	   VMOV.F32 r0,s27
-	   BL printMsg
-	   BL printNewLine 
-	   MOV r14,r5
-	   BX lr
-	   LTORG
+		; 
+		
 	   ENDFUNC
 	   
 	   EXPORT __GateNand	 
 __GateNand function
-	   MOV r5,r14
+	   MOV r4,r14
 	   VCVTR.S32.F32 s21,s7
 	   VCVTR.S32.F32 s22,s8
 	   VCVTR.S32.F32 s23,s9
-		; weights
+		; The weights are stored here below
 	   VLDR.F32 s11, =-3.0
 	   VLDR.F32 s12, =-3.0
 	   VLDR.F32 s13, =-3.0
@@ -121,10 +138,14 @@ __GateNand function
 	   VMUL.F32 s13,s13,s9 ; weight3*x3
 	   VMUL.F32 s14,s14,s10 ; weight4*bias
 	   
+	   ;adding S11, S12, S13 , S14
 	   VADD.F32 s15,s11,s12 ;
 	   VADD.F32 s15,s15,s13 ;
 	   VADD.F32 s15,s15,s14 ;
 	   VMOV.F32 s2,s15
+
+	   ; Printing the input and the out put for checking the values correct or not
+	   ; The output is in S28
 	   BL __sigmoid
 	   VCVTR.S32.F32 s28,s6
 	   VMOV.F32 r0,s21
@@ -136,18 +157,18 @@ __GateNand function
 	   VMOV.F32 r0,s28
 	   BL printMsg
 	   BL printNewLine
-	   MOV r14,r5
+	   MOV r14,r4
 	   BX lr
 	   LTORG
 	   ENDFUNC
 	   
 	   EXPORT __GateNor	 
 __GateNor function
-	   MOV r5,r14
+	   MOV r4,r14
 	   VCVTR.S32.F32 s21,s7
 	   VCVTR.S32.F32 s22,s8
 	   VCVTR.S32.F32 s23,s9
-		;weights
+		; The weights are stored here below
 	   VLDR.F32 s11, =-2.0
 	   VLDR.F32 s12, =-2.0
 	   VLDR.F32 s13, =-2.0
@@ -157,11 +178,14 @@ __GateNor function
 	   VMUL.F32 s13,s13,s9 ; weight3*x3
 	   VMUL.F32 s14,s14,s10 ; weight4*bias
 	   
+	   ;adding S11, S12, S13 , S14
 	   VADD.F32 s15,s11,s12 ;
 	   VADD.F32 s15,s15,s13 ;
 	   VADD.F32 s15,s15,s14 ;
 	   VMOV.F32 s2,s15
-	   
+
+	   ; Printing the input and the out put for checking the values correct or not
+	   ; The output is in S29
 	   BL __sigmoid
 	   VCVTR.S32.F32 s29,s6
 	   VMOV.F32 r0,s21
@@ -173,14 +197,14 @@ __GateNor function
 	   VMOV.F32 r0,s29
 	   BL printMsg
 	   BL printNewLine
-	   MOV r14,r5
+	   MOV r14,r4
 	   BX lr
 	   LTORG
 	   ENDFUNC
 	   
 	   EXPORT __GateXor	 
 __GateXor function
-	   MOV r5,r14
+	   MOV r4,r14
 	   VCVTR.S32.F32 s21,s7
 	   VCVTR.S32.F32 s22,s8
 	   VCVTR.S32.F32 s23,s9
@@ -195,6 +219,7 @@ __GateXor function
 	   VMUL.F32 s13,s13,s9 ; weight3*x3
 	   VMUL.F32 s14,s14,s10 ; weight4*bias
 	   
+	   ;adding S11, S12, S13 , S14
 	   VADD.F32 s15,s11,s12 ;
 	   VADD.F32 s15,s15,s13 ;
 	   VADD.F32 s15,s15,s14 ;
@@ -277,7 +302,9 @@ __GateXor function
 	   VADD.F32 s15,s15,s16 
 	   VADD.F32 s15,s15,s14 
 	   VMOV.F32 s2,s15
-	   
+
+	   ; Printing the input and the out put for checking the values correct or not
+	   ; The output is in S30
        BL __sigmoid 	;The below is Hidden Layer-2 Y1 output
 	   VCVTR.S32.F32 s30,s6
 	   VMOV.F32 r0,s21
@@ -289,18 +316,19 @@ __GateXor function
 	   VMOV.F32 r0,s30
 	   BL printMsg
 	   BL printNewLine
-	   MOV r14,r5
+	   MOV r14,r4
 	   BX lr
 	   LTORG
 	   ENDFUNC
 	   
 	   EXPORT __GateXnor	 
 __GateXnor function
-	   MOV r5,r14
+	   MOV r4,r14
 	   VCVTR.S32.F32 s21,s7
 	   VCVTR.S32.F32 s22,s8
 	   VCVTR.S32.F32 s23,s9
-		;weights
+		
+		; The weights are stored here below
 	   VLDR.F32 s11, =2.0
 	   VLDR.F32 s12, =-2.0
 	   VLDR.F32 s13, =2.0
@@ -396,7 +424,9 @@ __GateXnor function
 	   VADD.F32 s15,s15,s13 
 	   VADD.F32 s15,s15,s16 
 	   VMOV.F32 s2,s15
-	   
+
+	   ; Printing the input and the out put for checking the values correct or not
+	   ; The output is in S31
        BL __sigmoid 	;The below is Hidden Layer-2 Y1 output
 	   VCVTR.S32.F32 s31,s6
 	   VMOV.F32 r0,s21
@@ -409,7 +439,7 @@ __GateXnor function
 	   BL printMsg
 	   BL printNewLine
 	   
-	   MOV r14,r5
+	   MOV r14,r4
 	   BX lr
 	   LTORG
 
@@ -418,11 +448,12 @@ __GateXnor function
 	   EXPORT __GateNot	 
 __GateNot function
 	   ;NOT weight - s11,s14
-	   MOV r5,r14
+	   MOV r4,r14
 	   VCVTR.S32.F32 s21,s7
 	   VCVTR.S32.F32 s22,s8
 	   VCVTR.S32.F32 s23,s9
 	   
+	   ; The weights are stored here below
 	   VLDR.F32 s11, =-2.0
 	   VLDR.F32 s14, =1.0
 	   
@@ -432,6 +463,8 @@ __GateNot function
 	   VADD.F32 s15,s11,s14 
 	   VMOV.F32 s2,s15
 	   
+	   ; Printing the input and the out put for checking the values correct or not
+	   ; The output is in S25
 	   BL __sigmoid 	;This is Hidden Layer-2 Y1 output
 	   VCVTR.S32.F32 s25,s6
 	   VMOV.F32 r0,s21
@@ -439,7 +472,7 @@ __GateNot function
 	   VMOV.F32 r0,s25
 	   BL printMsg
 	   BL printNewLine
-       MOV r14,r5
+       MOV r14,r4
 	   BX lr
 	   LTORG
 	   ENDFUNC
@@ -449,6 +482,13 @@ __GateNot function
 __main  function		 
 	   ; Output is stored in here -- s25-NOT, s26-AND, s27-OR, s28-NAND, s29-NOR, s30-XOR, s31-XNOR
        ; The inputs are in s7,s8,s9
+	   
+	   ; I am printing the output and calling the Printmsg in the gates function itself
+	   
+	   ; The below is the codes for printing the truth table for checking the output
+	   
+	   
+	   ;Priniting NOT gate truth table
 	   MOV r0,#0
 	   BL printGate
 
@@ -460,7 +500,7 @@ __main  function
 	   VLDR.F32 s10, =1.0 ; bias to the input
 	   BL __GateNot
 	   
-	   
+	   ;Priniting AND gate truth table
 	   MOV r0,#1
 	   BL printGate
 
@@ -495,6 +535,7 @@ __main  function
 	   BL __GateAnd
 	   
 	   
+	   ;Priniting OR gate truth table
 	   MOV r0,#2
 	   BL printGate
 
@@ -529,6 +570,7 @@ __main  function
 	   VLDR.F32 s10, =1.0 ; bias to the input
 	   BL __GateOr
 	   
+	   ;Priniting NAND gate truth table
 	   MOV r0,#3
 	   BL printGate
 
@@ -562,6 +604,7 @@ __main  function
 	   VLDR.F32 s10, =1.0 ; bias to the input
 	   BL __GateNand
 	   
+	   ;Priniting NOR gate truth table
 	   MOV r0,#4
 	   BL printGate
 
@@ -596,6 +639,7 @@ __main  function
 	   VLDR.F32 s10, =1.0 ; bias to the input
 	   BL __GateNor
 	   
+	   ;Priniting XOR gate truth table
 	   MOV r0,#5
 	   BL printGate
 
@@ -630,6 +674,7 @@ __main  function
 	   VLDR.F32 s10, =1.0 ; bias to the input
 	   BL __GateXor
 	   
+	   ;Priniting XNOR gate truth table
 	   MOV r0,#6
 	   BL printGate
 
